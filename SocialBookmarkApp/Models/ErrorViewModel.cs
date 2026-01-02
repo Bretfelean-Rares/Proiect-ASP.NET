e13e78e19c137cfe89ae59aae4724cc18a5a7995
Bretfelean Rares <91412805+Bretfelean-Rares@users.noreply.github.com>
2026-01-02 20:51:39 +0200

    refac proiectul

--- SocialBookmarkApp/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace SocialBookmarkApp.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
