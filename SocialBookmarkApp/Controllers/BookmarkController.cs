e67345fef3f5372ed4a03a6ff462384e4a5f94ef
Bretfelean Rares <91412805+Bretfelean-Rares@users.noreply.github.com>
2026-01-03 20:33:45 +0200

    Adauagat Index Bookmarks

--- SocialBookmarkApp/Controllers/BookmarkController.cs
+++ SocialBookmarkApp/Controllers/BookmarkController.cs
@@ -1,12 +1,36 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SocialBookmarkApp.Data;
+using SocialBookmarkApp.Models;
 
 namespace SocialBookmarkApp.Controllers;
 
-public class BookmarkController : Controller
+public class BookmarkController(AppDbContext context, UserManager<ApplicationUser> userManager) : Controller
 {
+    
+    private readonly AppDbContext db = context;
+
     // GET
+    [AllowAnonymous]
     public IActionResult Index()
     {
+        var bookmarks = db.Bookmarks
+            .Include(b => b.User)
+            .OrderByDescending(b => b.CreatedAt)
+            .Where(b => b.IsPublic);
+
+        ViewBag.Bookmarks = bookmarks;
+
+        if (TempData.ContainsKey("message"))
+        {
+            ViewBag.Message = TempData["message"];
+            ViewBag.Alert = TempData["messageType"];
+        }
+
         return View();
     }
+    
+    
 }
--- SocialBookmarkApp/obj/Debug/net9.0/SocialBookmarkApp.AssemblyInfo.cs
+++ SocialBookmarkApp/obj/Debug/net9.0/SocialBookmarkApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialBookmarkApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a02c43ad39331c6de1835053a9317bc65c95598")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+483cde9dbe5ac071c5ddcfd1bc57ffdb8b38dad1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialBookmarkApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialBookmarkApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
